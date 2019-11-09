aa1f43ef6ff81478a09d80d944615525215ee135
dashaLavrukh <kiit15.dlavrukh@gmail.com>
2019-11-09 11:43:41 +0200

    Lectures notes

---
+++ src/main/java/com/company/course/lectures/lecture_2/Lecture_02.md
@@ -0,0 +1,9 @@
+# Lecture 2. Condition
+### 1 if .. else
+<Simple if>
+if (boolean_expretion) {
+    ***
+    expressions
+    ***
+    }
+    
---
+++ src/main/java/com/company/course/lectures/lecture_2/StayHydrated.java
@@ -0,0 +1,7 @@
+package com.company.course.lectures.lecture_2;
+
+public class StayHydrated {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
