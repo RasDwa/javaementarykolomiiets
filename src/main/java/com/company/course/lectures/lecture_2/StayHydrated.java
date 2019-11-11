8ec41c333f963a8396c65e6a563630478e82bdb2
RasDwa <opera1406@gmail.com>
2019-11-11 16:28:36 +0200

    Update StayHydrated.java

--- src/main/java/com/company/course/lectures/lecture_2/StayHydrated.java
+++ src/main/java/com/company/course/lectures/lecture_2/StayHydrated.java
@@ -17,3 +17,25 @@ public class MyClass {
      
     }
 }
+***************************************************
+public class MyClass {
+    public static void main(String args[]) {
+    
+     int a = 25;
+     if(x < 12){
+         System.out.println("Вы ребёнок");
+     }
+     if((x > 13) && (x < 17)){
+         System.out.println("Вы подросток");
+     }
+     if((x > 18) && (x < 64)){
+         System.out.println("Вы взрослый");
+     }
+     if(x > 65){
+         System.out.println("Вы пожилой");
+     }
+      
+   // System.out.println(q+1);
+   
+    }
+}
