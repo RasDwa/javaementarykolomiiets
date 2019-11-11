32dd137bfee10dad7af98737c71e1e9c0c3026a6
RasDwa <opera1406@gmail.com>
2019-11-11 15:31:56 +0200

    Update StayHydrated.java

--- src/main/java/com/company/course/lectures/lecture_2/StayHydrated.java
+++ src/main/java/com/company/course/lectures/lecture_2/StayHydrated.java
@@ -1,7 +1,19 @@
-package com.company.course.lectures.lecture_2;
+public class MyClass {
+    public static void main(String args[]) {
+      double x = 1705.0;
+      double k = x/100;
+      
+      if (k % 1 == 0) {
+    //целое
+    int c = (int)k;
+    System.out.println(c);
+    }
+    else {
+        int q = (int)k;
+    System.out.println(q+1);
+        
+    }
 
-public class StayHydrated {
-    public static void main(String[] args) {
-        System.out.println("Hello word");
+     
     }
 }
